5917cce0403dc07d6ae12e180c19fc7459f9c9f2
shusweela <shusweela@gmailcom>
2020-08-22 02:10:49 -0500

    turn in HW;

---
+++ RPSgame.js
@@ -0,0 +1,29 @@
+//Sends player to computer function based on button selected//
+if (button == rockButton){select Rock;}
+else if button == select Paper; 
+else if {button == scissButton;}
+else {display "Are you still there", name
+}
+
+//function that randomly selects a number//
+
+//Function if player selects Rocks//
+function Rock (Math.floor(Math.random() * 4) +1);{
+if Rock == 1 return ("Its a draw!");{
+    else if Rock == 2 return ("You lose!  Try again!");{
+        else Rock == 3, return (name," " "," "you won!");
+    }}
+
+//Function if player selects Paper//
+function (Paper) (Math.floor(Math.random() * 4) +1);{
+   if Paper == 2 return ("Its a draw!");{
+        else if Paper == 3 return ("You lose!  Try again!");{
+            else Paper == 1, return (name," " "," "you won!");
+        }}
+
+//Function for if player selects Scissors//
+function Scissors (Math.floor(Math.random() * 4) +1);{
+    Scissors == 3 return ("Its a draw!");{
+        else if Scissors == 1 return ("You lose!  Try again!");{
+            else Scissors == 2, return (name," " "," "you won!");
+        }}
